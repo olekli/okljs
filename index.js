274609b5d0ca507ba757cbc291fd7b7100b75bbc
Ole Kliemann <mail@olekliemann.de>
2022-03-19 20:41:20 +0100

    Add default logger via winston

--- index.js
+++ index.js
@@ -4,5 +4,7 @@
 "use strict";
 
 const { assert } = require("./lib/assert.js");
+const { makeLogger } = require("./lib/logger.js");
 
 module.exports.assert = assert;
+module.exports.makeLogger = makeLogger;
