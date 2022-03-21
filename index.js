3d5491d16126a0c0bdb3d0efbeef1de3aa3d6911
Ole Kliemann <mail@olekliemann.de>
2022-03-21 17:00:28 +0100

    Add functions to handle results

--- index.js
+++ index.js
@@ -5,6 +5,17 @@
 
 const { assert } = require("./lib/assert.js");
 const { makeLogger } = require("./lib/logger.js");
+const { make_result, match_result, ok, err, make_ok, make_err, get_ok, get_err } = require("./lib/result.js");
+const { addLazyProperty } = require("./lib/lazy_property.js");
 
 module.exports.assert = assert;
 module.exports.makeLogger = makeLogger;
+module.exports.make_result = make_result;
+module.exports.match_result = match_result;
+module.exports.make_ok = make_ok;
+module.exports.make_err = make_err;
+module.exports.ok = ok;
+module.exports.err = err;
+module.exports.get_ok = get_ok;
+module.exports.get_err = get_err;
+module.exports.addLazyProperty = addLazyProperty;
