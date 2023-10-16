cdb0483e0008dd81c001f89fca4ed1378585d036
Ole Kliemann <mail@olekliemann.de>
2023-10-16 15:00:51 +0000

    Add unzip_result

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 const { assert } = require("./lib/assert.js");
 const { makeLogger } = require("./lib/logger.js");
-const { make_result, match_result, ok, err, make_ok, make_err, get_ok, get_err, useJestResultMatcher, map_result, unwrap_result } = require("./lib/result.js");
+const { make_result, match_result, ok, err, make_ok, make_err, get_ok, get_err, useJestResultMatcher, map_result, unwrap_result, unzip_result } = require("./lib/result.js");
 const { addLazyProperty } = require("./lib/lazy_property.js");
 const { match_enum } = require("./lib/enum.js");
 const { readJsons } = require("./lib/read_jsons.js");
@@ -20,6 +20,7 @@ module.exports.make_result = make_result;
 module.exports.match_result = match_result;
 module.exports.map_result = map_result;
 module.exports.unwrap_result = unwrap_result;
+module.exports.unzip_result = unzip_result;
 module.exports.make_ok = make_ok;
 module.exports.make_err = make_err;
 module.exports.ok = ok;
